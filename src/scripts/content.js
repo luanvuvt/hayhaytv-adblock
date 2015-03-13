a190d3a2f98bd4c9f93504a63d4df2193f01e30e
Luan Vu <luanvuvt@gmail.com>
2015-03-14 00:36:04 +0700

    Updated to block ads

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.textContent = 'ADS = false;';
+script.textContent = "ADS_YO = false; document.getElementById('btn_xemngay').click();";
 
 document.body.appendChild(script);
