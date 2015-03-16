36c98b8aa6bca97cc892d1d6da7b8cd8ef5fa55f
Luan Vu <luanvuvt@gmail.com>
2015-03-16 23:33:30 +0700

    v1.0.5

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,5 +1,50 @@
+function hhRemoveAds() {
+    $("#btn_xemngay")
+    .off('click')
+    .click(function () {
+	    $(".banner_player_img").hide();
+	        
+	    $(".info_film-div").animate({
+	        height: "0px",
+	        opacity: 0
+	    }, {
+	        duration: 1500,
+	        complete: function () {
+	            $(".info_film-div").hide();
+	        }
+	    });
+
+	    $.ajax({
+	        type: 'POST',
+	        url: http + 'vod/addview',
+	        data: "id=" + "34383938316E61" + "&q=" + "episode",
+	        dataType: 'json',
+	        success: function (result) {
+
+	        }
+	    })
+
+	    addPlayer(initVideoUrl, imageSrc, adsXMLURL, infoXMLUrl, videoSubs);
+	    // addPlayer(initVideoUrl, imageSrc , 'http://www.hayhaytv.vn/ads.xml', videoSubs);
+	    button = 2;
+	    if(isVTT){
+	        api_impression_hit(234,39);
+	    }
+	    
+	     //increase z-index
+	    var balloonContainer = $('.AdvertiseBalloonFooterBottomRight div');
+	    if(balloonContainer.length){
+	         balloonContainer.each(function(index, item){
+	            $(item).css({"z-index" : '99998 !important'});
+	        });
+	         $('.banner_player ').css({position: "absolute","z-index" : '99999'});
+	    }
+	});
+}
+
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.textContent = "ADS_YO = false; document.getElementById('btn_xemngay').click();";
+
+script.textContent = hhRemoveAds.toString() + ' hhRemoveAds();';
 
 document.body.appendChild(script);
